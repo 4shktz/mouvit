0e98bec8b05794e7c5e176af9fa0277d37276400
4shktz <evilancasteras@edenschool.fr>
2023-12-20 09:58:48 +0100

    new bonus version

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener('keydown', function(e) {
     let cursor = document.querySelector('#cursor');
     let left = parseInt(cursor.style.left) || 0;
     let top = parseInt(cursor.style.top) || 0;
-  
+
     if (e.key === 'ArrowLeft') {
         left -= 10;
         console.log('gauche');
@@ -19,9 +19,11 @@ document.addEventListener('keydown', function(e) {
         top += 10;
         console.log('bas');
     }
-  
+
     cursor.style.left = left + 'px';
     cursor.style.top = top + 'px';
+
+    let bonus = document.querySelector('#bonus');
 
     function HeightBonus() {
         let bonus = document.createElement('div');
@@ -29,14 +31,19 @@ document.addEventListener('keydown', function(e) {
         bonus.style.left = Math.floor(Math.random() * window.innerWidth) + 'px';
         bonus.style.top = Math.floor(Math.random() * window.innerHeight) + 'px';
         document.body.appendChild(bonus);
-    
-        bonus.addEventListener('click', () => {
+    }
+
+    if (bonus) {
+        let bonusLeft = parseInt(bonus.style.left) || 0;
+        let bonusTop = parseInt(bonus.style.top) || 0;
+
+        if (Math.abs(left - bonusLeft) <= 10 && Math.abs(top - bonusTop) <= 10) {
             cursor.style.width = parseInt(cursor.style.width) + 10 + 'px';
             cursor.style.height = parseInt(cursor.style.height) + 10 + 'px';
             document.body.removeChild(bonus);
-        });
+        }
     }
-    
+
     if (Math.random() < 0.01) {
         HeightBonus();
     }
