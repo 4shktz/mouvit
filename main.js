9431aca0c3624c0f58bc0b1711a1b120033b9599
4shktz <evilancasteras@edenschool.fr>
2023-12-20 10:00:44 +0100

    change if by switch

--- main.js
+++ main.js
@@ -3,21 +3,25 @@ document.addEventListener('keydown', function(e) {
     let left = parseInt(cursor.style.left) || 0;
     let top = parseInt(cursor.style.top) || 0;
 
-    if (e.key === 'ArrowLeft') {
-        left -= 10;
-        console.log('gauche');
-    }
-    if (e.key === 'ArrowRight') {
-        left += 10;
-        console.log('droite');
-    }
-    if (e.key === 'ArrowUp') {
-        top -= 10;
-        console.log('haut');
-    }
-    if (e.key === 'ArrowDown') {
-        top += 10;
-        console.log('bas');
+    switch (e.key) {
+        case 'ArrowLeft':
+            left -= 10;
+            console.log('gauche');
+            break;
+        case 'ArrowRight':
+            left += 10;
+            console.log('droite');
+            break;
+        case 'ArrowUp':
+            top -= 10;
+            console.log('haut');
+            break;
+        case 'ArrowDown':
+            top += 10;
+            console.log('bas');
+            break;
+        default:
+            break;
     }
 
     cursor.style.left = left + 'px';
